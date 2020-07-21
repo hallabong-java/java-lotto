9f71cbadc2fa4f016c3a5c7ba8f7e77e8039facf
dusdn1702 <dusdn1702@naver.com>
2020-07-21 17:10:23 +0900

    feat : 당첨 번호 가진 객체 WinnerLotto 생성

---
+++ src/main/java/me/ywoo/domain/WinnerLotto.java
@@ -0,0 +1,32 @@
+package me.ywoo.domain;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class WinnerLotto {
+
+    private static final String DELIMITER = ", ";
+    public static final ArrayList<Integer> winnerNumbers = new ArrayList<Integer>();
+
+    public WinnerLotto() { }
+
+    public WinnerLotto(final String inputNumbers) {
+        findDuplication(createWinnerNumbers(inputNumbers));
+    }
+
+    public ArrayList<Integer> createWinnerNumbers(final String inputNumbers) {
+        Objects.requireNonNull(inputNumbers, "숫자가 null입니다.");
+
+        for(String numberText : inputNumbers.split(DELIMITER)){
+            Integer number = Integer.parseInt(numberText);
+            winnerNumbers.add(number);
+        }
+        return winnerNumbers;
+    }
+
+    public void findDuplication(ArrayList<Integer> winnerNumbers){
+        if(winnerNumbers.stream().distinct().collect(Collectors.toList()).size() != winnerNumbers.size())
+            throw new IllegalArgumentException("값이 중복되지 않게 입력해야 합니다.");
+    }
+}
---
+++ src/test/java/me/ywoo/domain/WinnerLottoTest.java
@@ -0,0 +1,44 @@
+package me.ywoo.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class WinnerLottoTest {
+
+    @DisplayName("createWinnerNumbers(String) - 문자열 받아서 자르고 Integer로 배열 생성")
+    @Test
+    void CreateWinnerNumbers_generateInstance() {
+        ArrayList<Integer> actual = new WinnerLotto().createWinnerNumbers("1, 2, 3, 4");
+
+        assertThat(actual).containsExactly(1, 2, 3, 4);
+    }
+
+    @DisplayName("createWinnerNumbers(String) - NULL 받은 경우 예외 검사")
+    @ParameterizedTest
+    @NullSource
+    void CreateWinnerNumbers_checkNullExceptionThrown(final String inputNumbers) {
+        assertThatThrownBy(() -> new WinnerLotto(inputNumbers))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("숫자가 null입니다.");
+    }
+
+    @DisplayName("findDuplication() - 중복되는 숫자 있는지 검사")
+    @Test
+    void findDuplication_checkDuplicationExceptionThrown() {
+        ArrayList<Integer> winnerNumbers = new ArrayList<Integer>();
+
+        winnerNumbers.addAll(Arrays.asList(13, 11, 20, 19, 40, 11));
+
+        assertThatThrownBy(() -> new WinnerLotto().findDuplication(winnerNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("값이 중복되지 않게 입력해야 합니다.");
+    }
+}
