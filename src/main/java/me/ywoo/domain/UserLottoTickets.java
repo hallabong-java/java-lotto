6e78213d83035511c1f2b89bfad4d8220ebfceec
dusdn1702 <dusdn1702@naver.com>
2020-07-24 13:23:57 +0900

    refactor : 매직넘버 상수화

--- src/main/java/me/ywoo/domain/UserLottoTickets.java
+++ src/main/java/me/ywoo/domain/UserLottoTickets.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class UserLottoTickets {
-    public ArrayList<RandomNumbers> lottoNumbers = new ArrayList<RandomNumbers>();
+    private static final int MINIMUM_TO_COUNT = 0;
+    public static ArrayList<RandomNumbers> lottoNumbers = new ArrayList<RandomNumbers>();
 
     public UserLottoTickets(int countOfLotto) {
-        while(countOfLotto-->0){
+        while (countOfLotto-- > MINIMUM_TO_COUNT) {
             this.lottoNumbers.add(new RandomNumbers());
         }
         Objects.requireNonNull(lottoNumbers, "발행된 로또 티켓이 없습니다.");
