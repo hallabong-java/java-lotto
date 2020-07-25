216a986978ebcd58e01d3c2dc67a2a3ebcf1bce4
dusdn1702 <dusdn1702@naver.com>
2020-07-25 19:44:57 +0900

    feat : main 함수 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,8 @@
+import me.ywoo.controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-
+        LottoController gameController = new LottoController();
+        gameController.run();
     }
 }
