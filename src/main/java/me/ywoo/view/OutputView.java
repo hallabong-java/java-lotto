17c5559f8b79a684ee44fd4b368ce9234633bd87
dusdn1702 <dusdn1702@naver.com>
2020-07-25 19:43:21 +0900

    refactor : 유저가 구입한 로또 출력 재구현

--- src/main/java/me/ywoo/view/OutputView.java
+++ src/main/java/me/ywoo/view/OutputView.java
@@ -6,6 +6,7 @@ import me.ywoo.domain.Rank;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.List;
 
 public class OutputView {
     public static void printCountOfLotto(int count) {
@@ -14,17 +15,18 @@ public class OutputView {
 
     public static void printTickets(ArrayList<RandomNumbers> UserLotto) {
         for (RandomNumbers randomNumbers : UserLotto) {
-            makeString(randomNumbers);
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(makeString(randomNumbers, stringBuilder));
+            System.out.println(stringBuilder);
         }
     }
 
-    public static void makeString(RandomNumbers randomNumbers) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("[");
-        for (Integer numbers : randomNumbers.randomNumbers) {
-            stringBuilder.append(String.join(", "));
+    public static List<String> makeString(RandomNumbers randomNumbers, StringBuilder stringBuilder) {
+        List<String> numbers = new ArrayList<>();
+        for(Integer lottoNumbers : randomNumbers.getRandomNumbers()){
+            numbers.add(lottoNumbers.toString());
         }
-        stringBuilder.append("]");
+        return numbers;
     }
 
     public static void printResult() {
@@ -34,6 +36,12 @@ public class OutputView {
 
     public static void printCountOfSameNumber(Rank rank, Integer counts) {
         System.out.println(String.format("%d개 일치 (%d원)- %d개",
+                rank.getCountOfSameNumber(), rank.getPriceOfLotto(), counts
+        ));
+    }
+
+    public static void printSecondRankCountOfSameNumber(Rank rank, Integer counts) {
+        System.out.println(String.format("%d개 일치, 보너스 볼 일치 (%d원)- %d개",
                 rank.getCountOfSameNumber(), rank.getPriceOfLotto(), counts
         ));
     }
