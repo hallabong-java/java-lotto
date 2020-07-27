05ccbbc8b555d8027b6f15edffb40208a2955cfb
dusdn1702 <dusdn1702@naver.com>
2020-07-27 15:23:37 +0900

    refactor : 메서드 분리

--- src/main/java/me/ywoo/controller/LottoController.java
+++ src/main/java/me/ywoo/controller/LottoController.java
@@ -4,27 +4,36 @@ import me.ywoo.domain.*;
 import me.ywoo.view.InputView;
 import me.ywoo.view.OutputView;
 
+import java.math.BigInteger;
 import java.util.Map;
 
 public class LottoController {
     public void run(){
+        buyLotto();
+    }
+
+    private void buyLotto(){
         Price price = new Price(InputView.receivePrice());
         CountsOfLotto countsOfLotto = new CountsOfLotto(price.getPrice());
         OutputView.printCountOfLotto(countsOfLotto.getCount());
-
         UserLottoTickets userLottoTickets = new UserLottoTickets(countsOfLotto.getCount());
         OutputView.printTickets(userLottoTickets.getLottoNumbers());
-
+        calculateResult(userLottoTickets, price.getPrice());
         OutputView.printOneLine();
+    }
+
+    private void calculateResult(UserLottoTickets userLottoTickets, int price){
         WinnerLotto winnerLotto = new WinnerLotto(InputView.receiveWinnerNumbers());
         BonusBall bonusBall = new BonusBall(InputView.receiveBonusBallNumber(), winnerLotto.getWinnerNumbers());
         ResultLotto resultLotto = new ResultLotto();
         Yield yield = new Yield();
         yield.calculateYield(yield.calculateTotalEarning(
-                userLottoTickets.getLottoNumbers(), winnerLotto.getWinnerNumbers(), bonusBall.getBonusBall(), resultLotto), price.getPrice());
-
-
+                userLottoTickets.getLottoNumbers(), winnerLotto.getWinnerNumbers(), bonusBall.getBonusBall(), resultLotto), price);
+        printResult(resultLotto, yield.getYield());
         OutputView.printOneLine();
+    }
+
+    private void printResult(ResultLotto resultLotto, BigInteger yield){
         OutputView.printResult();
         for(Map.Entry<Rank, Integer> entry : resultLotto.getResult().entrySet()){
             if(!entry.getKey().equals(Rank.SECOND) && !entry.getKey().equals(Rank.NOTHING)){
@@ -35,6 +44,6 @@ public class LottoController {
             }
 
         }
-        OutputView.printYield(yield.getYield());
+        OutputView.printYield(yield);
     }
 }
